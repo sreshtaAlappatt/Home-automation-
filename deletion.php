ab362221422ad0a5affbf74c90c0b46577ce76b1
SRESHTA JOHN ANTONY <48003302+sreshtaAlappatt@users.noreply.github.com>
2026-02-09 19:29:11 +0100

    Update and rename del.php to deletion.php

--- deletion.php
+++ deletion.php
@@ -2,5 +2,6 @@
 $con=mysqli_connect("localhost","root","","time");
 $n=$_GET['id'];
 $q=mysqli_query($con,"delete from name where id='$n'");
-header('Location:new8.php');
+header('Location:connection.php');
+
 ?>
